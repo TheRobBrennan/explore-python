994cf4f6a5bb1c31c5524b61a1f3f0663eac1e87
Rob Brennan <rob@therobbrennan.com>
2022-12-20 01:08:04 -0800

    Add comments about generating our plot

--- plot.py
+++ plot.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 import numpy as np
 
 x = np.linspace(0, 20, 100)
 plt.plot(x, np.sin(x))
-plt.show()
+
+# OPTIONAL: Save our plot to a PNG file
 # plt.savefig('./standardplot-example.png')
+
+# Show our graph
+plt.show()
